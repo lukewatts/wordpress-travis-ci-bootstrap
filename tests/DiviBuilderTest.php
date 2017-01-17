24f7bc4da908e033f16f27c50a27432702f4177b
Luke Watts <luke@luke-watts.com>
2017-01-17 10:44:21 +0000

    Adding tests directory

---
+++ tests/DiviBuilderTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace WordPressTravisCI\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class DiviBuilderTest extends TestCase
+{
+    public function testSomething()
+    {
+        $x = 1 + 1;
+        $this->assertTrue($x === 2);
+    }
+}
